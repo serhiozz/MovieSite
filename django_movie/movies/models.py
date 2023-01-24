fcf68d62fadfdb73ce6d9bbb97f43c8bebedbad2
Sergey <cergio78@gmail.com>
2023-01-24 17:39:32 +0700

    End lesson 9

--- django_movie/movies/models.py
+++ django_movie/movies/models.py
@@ -1,3 +1,127 @@
-from django.db import models
+from datetime import date
 
-# Create your models here.
+from django.db import models
+from django.urls import reverse
+
+
+class Category(models.Model):
+    '''Категории'''
+    name = models.CharField('Категория', max_length=150)
+    description = models.TextField('Описание', blank=True)
+    url = models.SlugField(max_length=160, unique=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
+
+class Actor(models.Model):
+    '''Актеры/Режиссеры'''
+    name = models.CharField('Имя', max_length=100)
+    age = models.PositiveSmallIntegerField('Возраст', default=0)
+    description = models.TextField('Описание', blank=True)
+    image = models.ImageField('Изображение', upload_to='actors/', blank=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Актеры и режиссеры'
+        verbose_name_plural = 'Актеры и режиссеры'
+
+class Genre(models.Model):
+    '''Жанры'''
+    name = models.CharField('Категория', max_length=150)
+    description = models.TextField('Описание', blank=True)
+    url = models.SlugField(max_length=160, unique=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Жанр'
+        verbose_name_plural = 'Жанры'
+
+class Movie(models.Model):
+    title = models.CharField('Название', max_length=100)
+    tagline = models.CharField('Слоган', max_length=100, default='')
+    description = models.TextField('Описание', blank=True)
+    poster = models.ImageField('Постер', upload_to='movies/', blank=True)
+    year = models.PositiveSmallIntegerField('Год выхода', default=2022)
+    country = models.CharField('Страна', max_length=30)
+    directors = models.ManyToManyField(Actor, verbose_name='Режиссер', related_name='film_director')
+    actors = models.ManyToManyField(Actor, verbose_name='Актер', related_name='film_actor')
+    genres = models.ManyToManyField(Genre, verbose_name='Жанры')
+    world_premiere = models.DateField('Премьера в мире', default=date.today)
+    budget = models.PositiveIntegerField('Бюджeт', default=0, help_text='Указать сумму в $')
+    fees_in_usa = models.PositiveIntegerField('Сборы в США', default=0, help_text='Указать сумму в $')
+    fees_in_world = models.PositiveIntegerField('Сборы в мире', default=0, help_text='Указать сумму в $')
+    category = models.ForeignKey(Category, verbose_name='Категория', on_delete=models.SET_NULL, null=True)
+    url = models.SlugField(max_length=160, unique=True)
+    draft = models.BooleanField('Черновик', default=False)
+
+    def __str__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        return reverse('movie_detail', kwargs={'slug': self.url})
+
+    class Meta:
+        verbose_name = 'Фильм'
+        verbose_name_plural = 'Фильмы'
+
+class MovieShots(models.Model):
+    '''Кадры из фильма'''
+    title = models.CharField('Название', max_length=150)
+    description = models.TextField('Описание', blank=True)
+    image = models.ImageField('Изображение', upload_to='movie_shots/', blank=True)
+    movie = models.ForeignKey(Movie, on_delete=models.CASCADE, verbose_name='Фильм')
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'Кадр из фильма'
+        verbose_name_plural = 'Кадры из фильма'
+
+class RatingStar(models.Model):
+    '''Звезда'''
+    value = models.PositiveSmallIntegerField('Значение', default=0)
+
+    def __str__(self):
+        return self.value
+
+    class Meta:
+        verbose_name = 'Звезда рейтинга'
+        verbose_name_plural = 'Звезды рейтинга'
+
+class Rating(models.Model):
+    '''Рейтинг'''
+    ip = models.CharField('IP адрес', max_length=15)
+    star = models.ForeignKey(RatingStar, on_delete=models.CASCADE, verbose_name='Звезда')
+    movie = models.ForeignKey(Movie, on_delete=models.CASCADE, verbose_name='Фильм')
+
+    def __str__(self):
+        return f'{self.star} - {self.movie}'
+
+    class Meta:
+        verbose_name = 'Рейтинг'
+        verbose_name_plural = 'Рейтинги'
+
+
+class Reviews(models.Model):
+    '''Отзывы'''
+    email = models.EmailField(null=True)
+    name = models.CharField('Имя', max_length=100)
+    text = models.TextField('Текст', max_length=5000)
+    parent = models.ForeignKey('self', verbose_name='Родитель', on_delete=models.SET_NULL, null=True, blank=True)
+    movie = models.ForeignKey(Movie, verbose_name='Фильм', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'{self.name} - {self.movie}'
+
+    class Meta:
+        verbose_name = 'Отзыв'
+        verbose_name_plural = 'Отзывы'
--- django_movie/movies/views.py
+++ django_movie/movies/views.py
@@ -1,3 +1,47 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.views import View
+from django.views.generic import ListView, DetailView
 
-# Create your views here.
+from .models import Movie
+from .forms import ReviewForm
+
+# class MoviesView(View):
+#     '''Список фильмов'''
+#     def get(self, request):
+#         movies = Movie.objects.all()
+#         return render(request, 'movies/movies.html', {'movie_list': movies})
+#
+# class MovieDetailView(View):
+#     """Полное описание фильма"""
+#     def get(self, request, slug):
+#         movie = Movie.objects.get(url=slug)
+#         return render(request, 'movies/movie_detail.html', {'movie': movie})
+
+class MoviesView(ListView):
+    model = Movie
+    template_name = 'movies/movie_list.html' # Можно без этого тега, если насзвать шаблон movie_list (имя модели + list (как класс)
+    queryset = Movie.objects.filter(draft=False)
+    context_object_name = 'movie_list'
+
+class MovieDetailView(DetailView):
+    model = Movie
+    template_name = 'movies/movie_detail.html' # Можно без этого тега (имя модели + detail (как класс)
+    context_object_name = 'movie'
+    slug_field = 'url' # Указываем по какому слагу искать, аналог в функции Movie.objects.get(url=slug)
+
+class AddReview(View):
+    '''Отзывы'''
+    def post(self, request, pk):
+        form = ReviewForm(request.POST)
+        # if form.is_valid():
+        #     form = form.save(commit=False)
+        #     form.movie_id = pk
+        #     form.save()
+        #     или
+        movie = Movie.objects.get(id=pk)
+        if form.is_valid():
+            form = form.save(commit=False)
+            form.movie = movie
+            form.save()
+
+        return redirect(movie.get_absolute_url())
